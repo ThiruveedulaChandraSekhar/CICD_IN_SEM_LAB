6a605fe6a0735afde194d4a6dfaf25f7490a41d8
ThiruveedulaChandraSekhar <163702568+ThiruveedulaChandraSekhar@users.noreply.github.com>
2025-10-25 07:59:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174, // 👈 Change to any port you want
+    port: 5174, 
   },
 })
